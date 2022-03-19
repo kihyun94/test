e529dab545cf3be90e2c0916fe7f0030e3dc0e69
kihyun94 <99324645+kihyun94@users.noreply.github.com>
2022-03-19 22:29:40 +0900

    Update bitcoinAutoTrade.py

--- bitcoinAutoTrade.py
+++ bitcoinAutoTrade.py
@@ -3,11 +3,9 @@ import time
 import datetime
 import pandas
 
-access = 
-secret = 
-
-upbit = pyupbit.Upbit(access, secret)
-print("Autotrade start")
+# 로그인
+upbit = pyupbit.Upbit("ZSXz6AcY9FlMhnrR6bM8K47NQhuJQgDRCMi9BOEQ","LOmkniz4QpEZMNj6QYlNL1XzuYGAtIpbotGtvtwo")
+print("autotrade start")
 
 
 def rsi(ohlc: pandas.DataFrame, period: int = 14):
@@ -27,38 +25,46 @@ coinlist = ["KRW-BTC",   "KRW-ETH", "KRW-EOS",  "KRW-SAND", "KRW-XRP",
             'KRW-TRX',   'KRW-MBL', 'KRW-CELO', 'KRW-KNC',  'KRW-BORA',
             'KRW-ADA',   'KRW-SRM', 'KRW-SOL',  'KRW-MFT',  'KRW-1INCH']
 
+
 lower28 = []
-higher70 = []
+lower23 = []
+lower18 = []
+sblower23 = []
+sblower18 = []
+higher65 = []
 
 # initiate
 for i in range(len(coinlist)):
     lower28.append(False)
-    higher70.append(False)
+    lower23.append(False)
+    lower18.append(False)    
+    sblower23.append(False)
+    sblower18.append(False)
+    higher65.append(False)
 
-def buy(coin):
-    money = upbit.get_balance("KRW")
-    if money < 20000 :
-        res = upbit.buy_market_order(coin, money)
-    elif money < 50000:
-        res = upbit.buy_market_order(coin, money*0.4)
-    elif money < 100000 :
-        res = upbit.buy_market_order(coin, money*0.3)
-    else :
-        res = upbit.buy_market_order(coin, money*0.2)
+def buy1(coin):
+    money = 20000
+    res = upbit.buy_market_order(coin, money)
     return
 
+def buy2(coin):
+    money = 20000
+    res = upbit.buy_market_order(coin, money)
+    return
+
+def buy3(coin):
+    money = 20000
+    res = upbit.buy_market_order(coin, money)
+    return
+
+# 시장가 매도 함수
 def sell(coin):
     amount = upbit.get_balance(coin)
     cur_price = pyupbit.get_current_price(coin)
     total = amount * cur_price
-    if total < 20000 :
+
+    if amount > 0.00001:
         res = upbit.sell_market_order(coin, amount)
-    elif total < 50000:
-        res = upbit.sell_market_order(coin, amount*0.4)
-    elif total < 100000:
-        res = upbit.sell_market_order(coin, amount*0.3)        
-    else :
-        res = upbit.sell_market_order(coin, amount*0.2)
     return
 
 while(True): 
@@ -66,18 +72,46 @@ while(True):
         for i in range(len(coinlist)):
             data = pyupbit.get_ohlcv(ticker=coinlist[i], interval="minute5")
             now_rsi = round(rsi(data, 14).iloc[-1],2)
-
-            if now_rsi <= 28 : 
+            
+            ###1차매수
+            if now_rsi <= 28 and sblower23[i] == False and sblower18[i] == False : 
                 lower28[i] = True
+                sblower23[i] = True
 
-            elif now_rsi >= 33 and lower28[i] == True:
-                buy(coinlist[i])
+            elif now_rsi >= 32 and lower28[i] == True:
                 lower28[i] = False
-            elif now_rsi >= 70 and higher70[i] == False:
+                buy1(coinlist[i])
+
+            ###1차매수 끝
+
+            ###2차매수
+            elif now_rsi <= 23 and sblower23[i] == True and sblower18[i] == False :
+                lower23[i] = True
+                sblower18[i] = True
+
+            elif now_rsi >= 28 and lower23[i] == True :
+                lower23[i] = False
+                buy2(coinlist[i])
+            ###2차매수 끝
+
+            ###3차매수
+            elif now_rsi <= 18 and sblower18[i] == True :
+                lower18[i] = True
+
+            elif now_rsi >= 23 and lower18[i] == True :
+                lower18[i] = False
+                buy3(coinlist[i])
+            ###3차매수끝
+
+            # 65이상이면 전량매도
+            elif now_rsi >= 65 and higher65[i] == False:           
                 sell(coinlist[i])
-                higher70[i] = True
-            elif now_rsi <= 60 :
-                higher70[i] = False
-            time.sleep(0.2)
+                higher65[i] = True
+                sblower23[i] = False
+            elif now_rsi <= 55 :
+                higher65[i] = False
+            #매도끝
+
+            time.sleep(0.5)
     except Exception as e:
         print(e)
