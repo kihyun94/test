fb3c8f01ff1bc8b7596bfb7dad715ae8584c628a
kihyun94 <99324645+kihyun94@users.noreply.github.com>
2022-03-19 18:55:27 +0900

    Update bitcoinAutoTrade.py

--- bitcoinAutoTrade.py
+++ bitcoinAutoTrade.py
@@ -1,60 +1,83 @@
-import time
 import pyupbit
+import time
 import datetime
+import pandas
 
-access = "FLuddP3VEdjVFWHA2Z6mBRd0htgxvxathBW4uHOD"
-secret = "8ABOzwL4F9nJs6x4uq3LvN4YvOQKGOxyi4xw2N2K"
+access = 
+secret = 
 
-def get_target_price(ticker, k):
-    """변동성 돌파 전략으로 매수 목표가 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="minute60", count=2)
-    target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
-    return target_price
-
-def get_start_time(ticker):
-    """시작 시간 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="minute60", count=1)
-    start_time = df.index[0]
-    return start_time
-
-def get_balance(ticker):
-    """잔고 조회"""
-    balances = upbit.get_balances()
-    for b in balances:
-        if b['currency'] == ticker:
-            if b['balance'] is not None:
-                return float(b['balance'])
-            else:
-                return 0
-    return 0
-
-def get_current_price(ticker):
-    """현재가 조회"""
-    return pyupbit.get_orderbook(ticker=ticker)["orderbook_units"][0]["ask_price"]
-
-# 로그인
 upbit = pyupbit.Upbit(access, secret)
-print("autotrade start")
+print("Autotrade start")
 
-# 자동매매 시작
-while True:
+
+def rsi(ohlc: pandas.DataFrame, period: int = 14):
+    delta = ohlc["close"].diff()
+    ups, downs = delta.copy(), delta.copy()
+    ups[ups < 0] = 0
+    downs[downs > 0] = 0
+
+    AU = ups.ewm(com = period-1, min_periods = period).mean()
+    AD = downs.abs().ewm(com = period-1, min_periods = period).mean()
+    RS = AU/AD
+
+    return pandas.Series(100 - (100/(1 + RS)), name = "RSI")  
+
+coinlist = ["KRW-BTC",   "KRW-ETH", "KRW-EOS",  "KRW-SAND", "KRW-XRP",
+            "KRW-WAVES", 'KRW-OMG', 'KRW-ETC',  'KRW-HUNT', 'KRW-STRK',
+            'KRW-TRX',   'KRW-MBL', 'KRW-CELO', 'KRW-KNC',  'KRW-BORA',
+            'KRW-ADA',   'KRW-SRM', 'KRW-SOL',  'KRW-MFT',  'KRW-1INCH']
+
+lower28 = []
+higher70 = []
+
+# initiate
+for i in range(len(coinlist)):
+    lower28.append(False)
+    higher70.append(False)
+
+def buy(coin):
+    money = upbit.get_balance("KRW")
+    if money < 20000 :
+        res = upbit.buy_market_order(coin, money)
+    elif money < 50000:
+        res = upbit.buy_market_order(coin, money*0.4)
+    elif money < 100000 :
+        res = upbit.buy_market_order(coin, money*0.3)
+    else :
+        res = upbit.buy_market_order(coin, money*0.2)
+    return
+
+def sell(coin):
+    amount = upbit.get_balance(coin)
+    cur_price = pyupbit.get_current_price(coin)
+    total = amount * cur_price
+    if total < 20000 :
+        res = upbit.sell_market_order(coin, amount)
+    elif total < 50000:
+        res = upbit.sell_market_order(coin, amount*0.4)
+    elif total < 100000:
+        res = upbit.sell_market_order(coin, amount*0.3)        
+    else :
+        res = upbit.sell_market_order(coin, amount*0.2)
+    return
+
+while(True): 
     try:
-        now = datetime.datetime.now()
-        start_time = get_start_time("KRW-XRP")
-        end_time = start_time + datetime.timedelta(minutes=60)
+        for i in range(len(coinlist)):
+            data = pyupbit.get_ohlcv(ticker=coinlist[i], interval="minute5")
+            now_rsi = round(rsi(data, 14).iloc[-1],2)
 
-        if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-XRP", 0.5)
-            current_price = get_current_price("KRW-XRP")
-            if target_price < current_price:
-                xrp = get_balance("KRW")
-                if xrp > 5000:
-                    upbit.buy_market_order("KRW-XRP", xrp*0.9995)
-        else:
-            btc = get_balance("XRP")
-            if xrp > 7:
-                upbit.sell_market_order("KRW-XRP", xrp*0.9995)
-        time.sleep(1)
+            if now_rsi <= 28 : 
+                lower28[i] = True
+
+            elif now_rsi >= 33 and lower28[i] == True:
+                buy(coinlist[i])
+                lower28[i] = False
+            elif now_rsi >= 70 and higher70[i] == False:
+                sell(coinlist[i])
+                higher70[i] = True
+            elif now_rsi <= 60 :
+                higher70[i] = False
+            time.sleep(0.2)
     except Exception as e:
         print(e)
-        time.sleep(1)
