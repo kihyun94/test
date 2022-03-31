550063a539ecfa694abf2546e66368f48be3aa34
kihyun94 <99324645+kihyun94@users.noreply.github.com>
2022-03-31 11:58:32 +0900

    Update bitcoinAutoTrade.py

--- bitcoinAutoTrade.py
+++ bitcoinAutoTrade.py
@@ -1,3 +1,26 @@
+import pyupbit
+import pandas
+
+upbit = pyupbit.Upbit(
+
+coinlist = ['KRW-ZIL', 'KRW-WAVES', 'KRW-VET', 'KRW-ETC', 'KRW-AAVE', 'KRW-CHZ', 'KRW-BTC', 'KRW-SRM', 'KRW-XRP', 'KRW-AERGO', 'KRW-ETH', 'KRW-BORA', 'KRW-QTUM', 'KRW-KAVA', 'KRW-SAND']
+
+lower = []
+
+avg123 =[[] for _ in range(100)]
+madocut =[[] for _ in range(100)]
+
+for i in range(len(coinlist)):
+    lower.append(False) 
+    for j in range(len(madocut)):
+        madocut[j].append(False) 
+    for j in range(len(avg123)):
+        avg123[j].append(0) 
+
+
+
+#RSI
+def rsi(ohlc: pandas.DataFrame, period: int = 14):
     delta = ohlc["close"].diff()
     ups, downs = delta.copy(), delta.copy()
     ups[ups < 0] = 0
@@ -9,100 +32,61 @@
 
     return pandas.Series(100 - (100/(1 + RS)), name = "RSI")  
 
-coinlist = ["KRW-BTC", "KRW-ETH", "KRW-EOS",  "KRW-SAND", "KRW-XRP",
-            "KRW-WAVES", 'KRW-OMG', 'KRW-ETC',  'KRW-HUNT', 'KRW-STRK',
-            'KRW-TRX',   'KRW-MBL', 'KRW-CELO', 'KRW-KNC',  'KRW-BORA',
-            'KRW-ADA',   'KRW-SRM', 'KRW-SOL',  'KRW-MFT',  'KRW-1INCH']
-
-
-lower28 = []
-lower23 = []
-lower18 = []
-sblower23 = []
-sblower18 = []
-higher65 = []
-
-# initiate
-for i in range(len(coinlist)):
-    lower28.append(False)
-    lower23.append(False)
-    lower18.append(False)    
-    sblower23.append(False)
-    sblower18.append(False)
-    higher65.append(False)
 
 def buy1(coin):
-    money = 20000
+    money = 30000  
     res = upbit.buy_market_order(coin, money)
     return
+
 
 def buy2(coin):
-    money = 20000
+    money = 30000  
     res = upbit.buy_market_order(coin, money)
     return
 
-def buy3(coin):
-    money = 20000
-    res = upbit.buy_market_order(coin, money)
-    return
 
 def sell(coin):
     amount = upbit.get_balance(coin)
     cur_price = pyupbit.get_current_price(coin)
-    total = amount * cur_price
 
     if amount > 0.00001:
         res = upbit.sell_market_order(coin, amount)
     return
 
+
+def get_avg(ticker):
+    balances = upbit.get_balances()
+    for b in balances:
+        if b['currency'] == ticker:
+            if b['avg_buy_price'] is not None:
+                return float(b['avg_buy_price'])
+            else:
+                return 0
+        return 0
+
+
+def get_balance(ticker):
+    
+    balances = upbit.get_balances()
+    for b in balances:
+        if b['currency'] == ticker:
+            if b['balance'] is not None:
+                return float(b['balance'])
+            else:
+                return 0
+    return 0
+
+############MAIN############
 while(True): 
     try:
         for i in range(len(coinlist)):
-            data = pyupbit.get_ohlcv(ticker=coinlist[i], interval="minute5")
-            now_rsi = round(rsi(data, 14).iloc[-1],2)
-          
+            data = pyupbit.get_ohlcv(ticker=coinlist[i], interval="minute5")         
+            now_rsi = round(rsi(data, 14).iloc[-1],2)                                
+            krw = get_balance("KRW")                                                 
+            avg_buy_price = upbit.get_avg_buy_price(coinlist[i])                     
+            amount = upbit.get_balance(coinlist[i])                                  
+            cur_price = pyupbit.get_current_price(coinlist[i])                       
+            
 
-            if now_rsi <= 28 and sblower23[i] == False and sblower18[i] == False : 
-                lower28[i] = True
-                sblower23[i] = True
-
-            elif now_rsi >=32 and lower28[i] == True:
-                lower28[i] = False
-                buy1(coinlist[i])
-                print("1SR MASU!")                
-
-
-            elif now_rsi <= 23 and sblower23[i] == True and sblower18[i] == False :
-                lower23[i] = True
-                sblower18[i] = True
-
-            elif now_rsi >= 28 and lower23[i] == True :
-                lower23[i] = False
-                buy2(coinlist[i])
-                print("2ND MASU!")                
-
-            elif now_rsi <= 18 and sblower18[i] == True :
-                lower18[i] = True
-
-            elif now_rsi >= 23 and lower18[i] == True :
-                lower18[i] = False
-                buy3(coinlist[i])
-                print("3RD MASU!")
-
-            elif now_rsi >= 65 and higher65[i] == False:           
-                sell(coinlist[i])
-                print("MADO !")                
-                higher65[i] = True
-                sblower23[i] = False
-            elif now_rsi <= 55 :
-                higher65[i] = False
-
-            print("NAME: ", coinlist[i], end=' ')
-            print("RSI :", now_rsi, end=' ')
-            if lower28[i] == True :
-                print(coinlist[i], "IS IN LIST", end='')
-            print()
-
-            time.sleep(0.5)
-    except Exception as e:
-        print(e)
+            if avg_buy_price == 0 :
+                avg_buy_price = 1      
